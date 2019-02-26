20345d4104ce642e888e7c3fb817f3cd6b788ab7
HUNGDT <hk2t.thaihung@gmail.com>
2019-02-26 19:37:21 +0700

    da lam den phan service, chua co popup

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {NavbarComponent} from './homepage/navbar/navbar.component';
+import {BannerComponent} from './homepage/banner/banner.component';
+import {ProductsComponent} from './homepage/products/products.component';
+import {AboutUsComponent} from './homepage/about-us/about-us.component';
+import {ServicesComponent} from './homepage/services/services.component';
+import { MapComponent } from './homepage/map/map.component';
+import { FooterComponent } from './homepage/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    BannerComponent,
+    ProductsComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    MapComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
